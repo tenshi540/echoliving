a79f75e619cf96144e2f5d823d924bef054b0fa2
Patrick Kirchknopf <pat.kir.1711@gmail.com>
2025-04-27 18:26:55 +0200

    minor update

--- frontend/compass.php
+++ frontend/compass.php
@@ -3,9 +3,9 @@
         <img src="/res/img/logo.png" alt="EchoLiving" style="height: 40px;">
     </div>
     <nav>
-    <a href="/index.php">Home</a>
+    <a href="/echoliving/frontend/index.php">Home</a>
     <a href="#">Products</a>
         <a href="#">Cart</a>
-        <a href="res/pages/login.php">Account</a> <!-- updated path -->
-    </nav>
+        <a href="/echoliving/frontend/res/pages/login.php">Account</a>
+        </nav>
 </header>
